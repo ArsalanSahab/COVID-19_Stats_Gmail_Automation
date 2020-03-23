806470769be4bfb634f2f90e34abdca306a33e99
Muhammad Arsalan <arsalan@Muhammads-MacBook-Pro-2.local>
2020-03-24 01:25:02 +0500

    Added and Edited covid_19.py file

--- covid_19.py
+++ covid_19.py
@@ -1,82 +1,119 @@
+######### IMPORTS #############
+import smtplib
+import time
+import schedule
 from selenium import webdriver
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from tabulate import tabulate
 
 
 
-class Covid_19():
+# INITIALISING COUNTRY AND CHROMEDRIVER
+Country = 'Country Name'
+driver = webdriver.Chrome('/Path/To/ChromeDriver')
 
-    def __init__(self):
+def get_data(Country):
+    
+    
+    driver.get('https://www.worldometers.info/coronavirus/')
+    
+    # Gettting Table Data
+    table = driver.find_element_by_xpath('//*[@id="main_table_countries_today"]/tbody[1]')
+    xpath = "//td[contains(text(), '%s')]"% Country
+    
+    # Check if country in Table
+    try:
 
-        self.driver = webdriver.Chrome('/Users/arsalan/Documents/chromedriver')
+        searched_country = table.find_element_by_xpath(xpath)
+        row = searched_country.find_element_by_xpath("./..")
 
-        def get_data(self):
+    except:
 
-        Country = "Pakistan" # Change to Desired
+        searched_country = table.find_element_by_link_text(Country)
+        row = searched_country.find_element_by_xpath("../..")
 
-        try:
+    col_data = row.find_elements_by_tag_name("td")
+    data_val = [x.text for x in col_data]
 
-            self.driver.get('https://www.worldometers.info/coronavirus/')
-            table = self.driver.find_element_by_xpath('//*[@id="main_table_countries"]/tbody[1]')
-            country_element = table.find_element_by_xpath("//td[contains(text(), 'China')]")
-            row = country_element.find_element_by_xpath("./..")
-            data = row.text.split(" ")
-            total_cases = data[1]
-            new_cases = data[2]
-            total_deaths = data[3]
-            new_deaths = data[4]
-            active_cases = data[5]
-            total_recovered = data[6]
-            serious_critical = data[7]
-            print("Country: " + country_element.text)
-            print("Total cases: " + total_cases)
-            print("New cases: " + new_cases)
-            print("Total deaths: " + total_deaths)
-            print("New deaths: " + new_deaths)
-            print("Active cases: " + active_cases)
-            print("Total recovered: " + total_recovered)
-            print("Serious, critical cases: " + serious_critical)
+    # Convert Data into a Table
+    data_keys = ['Country', 'Total Cases', 'New Cases', 'Total Deaths', 'New Deaths','Total Recovered','Active Cases','Critical','Total Cases per million']
+    data_dict = dict(zip(data_keys,data_val))
+    
+    return data_dict
 
-            send_mail(country_element.text, total_cases, new_cases, total_deaths, new_deaths, active_cases, total_recovered, serious_critical)
-
-            self.driver.close()
-
-        except:
-
-            self.driver.quit()
-
-    def send_mail(country_element, total_cases, new_cases, total_deaths, new_deaths, active_cases, total_recovered, serious_critical):
+def send_mail(data_dict):
+    
+    # Gmail Setup
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
     server.ehlo()
+    
+    server.login('email adrress', 'password')
 
-    server.login('email', 'password')
+    # Recipients to send mail to
+    recipients = ['list','of','recipients']
 
-    subject = 'Coronavirus stats in your country today!'
 
-    body = 'Today in ' + country_element + '\
-        \nThere is new data on coronavirus:\
-        \nTotal cases: ' + total_cases +'\
-        \nNew cases: ' + new_cases + '\
-        \nTotal deaths: ' + total_deaths + '\
-        \nNew deaths: ' + new_deaths + '\
-        \nActive cases: ' + active_cases + '\
-        \nTotal recovered: ' + total_recovered + '\
-        \nSerious, critical cases: ' + serious_critical  + '\
-        \nCheck the link: https://www.worldometers.info/coronavirus/'
+    for recipient in recipients :
+    
+            # Subject
+            subject = 'COVID-19 stats in your Country'
+            
+            # Body
+            text = """ COVID-19 Latest Statistics in your Country (Note : This is an Automated Email)
+            
+            {table}
+            Regards,
+            YourName"""
+            
+            html = """
+            <html>
+            <head>
+            <style> 
+            table, th, td {{ border: 1px solid black; border-collapse: collapse; }}
+            th, td {{ padding: 5px; }}
+            </style>
+            </head>
+            <body><p> COVID-19 Latest Statistics in your Country (Note : This is an automated email)
+            
+            </p>
+            {table}
+            <p>Regards,</p>
+            <p>Yourname</p>
+            </body></html>
+            """
+            
+            text = text.format(table=tabulate(data_dict.items(), tablefmt="grid"))
+            html = html.format(table=tabulate(data_dict.items(), tablefmt="html"))
+            
+            message = MIMEMultipart(
+                "alternative", None, [MIMEText(text), MIMEText(html,'html')])
 
-    msg = f"Subject: {subject}\n\n{body}"
+            
+            message['Subject'] = subject
+            message['From'] = 'your mail address' # Sender Mail
+            message['To'] = recipient # Reciever Mail
 
-    server.sendmail(
-        'Coronavirus',
-        'email',
-        msg
-    )
-    print('Hey Email has been sent!')
+            
+            server.sendmail(
+                message['From'],
+                message['To'],
+                message.as_string()
+            )
+            print('Email has been sent!')
 
     server.quit()
 
+def initiate_covid():
+    data_dict = get_data(Country)
+    send_mail(data_dict) 
+    return
 
+#scheduled for every 4 hours
+schedule.every(4).hours.do(initiate_covid())
 
-corona_bot = Covid_19()
-
-corona_bot.driver.get('https://www.worldometers.info/coronavirus/')
+while True:
+    schedule.run_pending()
+    time.sleep(1) 
